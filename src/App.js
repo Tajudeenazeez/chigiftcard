784832a4abeeec4dfd420fa26722ebd73caf6f7d
tajudeenazeez <azeezproto@gmail.com>
2023-01-11 15:11:23 +0100

    productPage with dummy dat and productList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
+import ProductLists from "./component/ProductLists";
 
 
 
 function App() {
   return (
-    <div>
+    <div className="h-[100vh] relative">
       <Header/>
+      {/* <div className="h-1/2 "></div> */}
+      <ProductLists/>
       <Footer/>
     </div>
   );
